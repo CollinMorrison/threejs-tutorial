251ab9d44aaa2a6bf15cb36e4b180373b2ff0c7c
Collin Morrison <morricollin@gmail.com>
2022-08-12 15:33:12 -0600

    general updates

--- index.js
+++ index.js
@@ -6,7 +6,7 @@
 import * as THREE from "three"
 const scene = new THREE.Scene()
 
-document.write("Hello World!")
+console.log("Hello World!")
 
 
 
@@ -104,4 +104,21 @@ function getCarFrontTexture() {
     context.fillRect(8, 8, 48, 24)
 
     return new THREE.CanvasTexture(canvas)
+}
+
+function getCarSideTexture() {
+    const canvas = document.createElement("canvas")
+    canvas.width = 128;
+    canvas.height = 32;
+    const context = canvas.getContext("2d")
+
+    context.fillStyle = "#666666"
+    context.fillRect(10, 8, 38, 24)
+    context.fillRect(58, 8, 60, 24)
+
+    return new THREE.CanvasTexture(canvas)
+}
+
+function createCar() {
+
 }
